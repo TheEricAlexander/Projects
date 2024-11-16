93a43ba8f594579e48a6b219351ea4326b94ab13
Eric Alexander <52408539+TheEricAlexander@users.noreply.github.com>
2024-11-16 14:41:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+// No import statement needed
 
 // Create a scene
 const scene = new THREE.Scene();
@@ -7,8 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.9, 1000);
 camera.position.z = 1.8;
 camera.position.y = 1.2; 
-camera.lookAt(0,0,0)
-//camera.setRotationFromAxisAngle(50)
+camera.lookAt(0, 0, 0);
 
 // Set up a renderer
 const renderer = new THREE.WebGLRenderer();
